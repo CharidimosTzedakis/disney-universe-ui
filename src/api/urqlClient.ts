a5ad061c4e178d46754f90798737752cfba43781
Charidimos Tzedakis <harris.jedakis@gmail.com>
2024-12-11 08:53:56 +0200

    installed urql devtools

--- src/api/urqlClient.ts
+++ src/api/urqlClient.ts
@@ -1,8 +1,12 @@
 import { Client, cacheExchange, fetchExchange } from "urql";
+import { devtoolsExchange } from "@urql/devtools";
+
+const isDev = import.meta.env.DEV;
+const exchanges = [cacheExchange, fetchExchange];
 
 const client = new Client({
-  url: "https://api.disneyapi.dev/graphql",
-  exchanges: [cacheExchange, fetchExchange],
+  url: import.meta.env.VITE_DISNEY_GRAPHQL_API,
+  exchanges: isDev ? [devtoolsExchange, ...exchanges] : [...exchanges],
 });
 
 export default client;
