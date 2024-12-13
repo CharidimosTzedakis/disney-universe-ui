62a118459a1fe8f0be1e7aff23abf1e9150b191d
Charidimos Tzedakis <harris.jedakis@gmail.com>
2024-12-14 00:36:28 +0200

    loading state for exportToExcelButton until the async exportToExcel completes

--- src/components/pieChart/exportToExcelButton.tsx
+++ src/components/pieChart/exportToExcelButton.tsx
@@ -1,18 +1,33 @@
+import { useState } from "react";
 import { Button } from "antd";
 import classes from "./charactersPieChart.module.scss";
 import { exportToExcel } from "./helpers";
 import type { FilmsPieChartDataPoint } from "@stores/chartStore";
 
-export default function exportToExcelButton({
+export default function ExportToExcelButton({
   dataForExport,
 }: {
   dataForExport: FilmsPieChartDataPoint[];
 }) {
+  const [loading, setLoading] = useState(false);
+
+  const handleClick = async () => {
+    setLoading(true);
+    try {
+      await exportToExcel(dataForExport);
+    } catch (error) {
+      console.error("An error occurred:", error);
+    } finally {
+      setLoading(false);
+    }
+  };
+
   return (
     <Button
       type="primary"
       className={classes.exportToExcelBtn}
-      onClick={() => exportToExcel(dataForExport)}
+      loading={loading}
+      onClick={handleClick}
     >
       Export to excel
     </Button>
