b729b802640b3fbdd5bdc1c6615a140a6b6e6c34
Charidimos Tzedakis <harris.jedakis@gmail.com>
2024-12-13 16:55:17 +0200

    consume chartStore

--- src/components/pieChart/charactersPieChart.tsx
+++ src/components/pieChart/charactersPieChart.tsx
@@ -1,11 +1,23 @@
 import Highcharts from "highcharts";
 import HighchartsReact from "highcharts-react-official";
+import useChartsStore from "@stores/chartStore";
 import { options } from "./pieChartConfig";
 
 export default function CharactersPieChart() {
+  const { filmsPieChart } = useChartsStore();
+
   return (
     <div>
-      <HighchartsReact highcharts={Highcharts} options={options} />
+      <HighchartsReact
+        highcharts={Highcharts}
+        options={{
+          ...options,
+          series: {
+            ...options.series,
+            data: filmsPieChart.data,
+          },
+        }}
+      />
     </div>
   );
 }
