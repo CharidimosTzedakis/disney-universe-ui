481e4f1697eb33f68b55b2eec40a74cd7288fa71
Charidimos Tzedakis <harris.jedakis@gmail.com>
2024-12-13 16:57:55 +0200

    keep pieChart test data as fixtures

---
+++ src/components/pieChart/fixtures.ts
@@ -0,0 +1,18 @@
+export const pieChartDataPoints = [
+  {
+    name: "Mikey",
+    y: 2,
+    films: ["Terminator", "Back to the future"],
+  },
+  {
+    name: "Donald",
+    y: 4,
+    films: ["Blade runner", "film2", "film3", "film4"],
+  },
+  {
+    name: "Taz",
+    y: 7,
+    films: ["film5", "film6", "film7", "film8", "film9", "film10", "film11"],
+  },
+  { name: "char1", y: 0, films: [] },
+];
--- src/components/pieChart/pieChartConfig.ts
+++ src/components/pieChart/pieChartConfig.ts
@@ -36,32 +36,7 @@ export const options = {
     {
       name: "Films",
       colorByPoint: true,
-      data: [
-        {
-          name: "Mikey",
-          y: 2,
-          films: ["Terminator", "Back to the future"],
-        },
-        {
-          name: "Donald",
-          y: 4,
-          films: ["Blade runner", "film2", "film3", "film4"],
-        },
-        {
-          name: "Taz",
-          y: 7,
-          films: [
-            "film5",
-            "film6",
-            "film7",
-            "film8",
-            "film9",
-            "film10",
-            "film11",
-          ],
-        },
-        { name: "char1", y: 0, films: [] },
-      ],
+      data: [],
     },
   ],
 };
