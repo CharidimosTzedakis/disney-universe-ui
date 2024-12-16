e7b01af1fd1a6df6c84c7d4a65e7c450871e9a2c
Charidimos Tzedakis <harris.jedakis@gmail.com>
2024-12-16 02:23:47 +0200

    charactersTable test cases

--- src/components/charactersTable/charactersTable.test.tsx
+++ src/components/charactersTable/charactersTable.test.tsx
@@ -4,8 +4,9 @@ import userEvent from "@testing-library/user-event";
 import { Provider } from "urql";
 import useSearchStore from "@stores/searchStore";
 import CharactersTable from "./index";
-import { fromValue } from "wonka";
+import { fromValue, never } from "wonka";
 import { characterItems } from "@test/fixtures";
+import useChartsStore from "@stores/chartStore.ts";
 
 describe("<CharactersTable />", () => {
   it("renders correctly", () => {
@@ -120,16 +121,124 @@ describe("<CharactersTable />", () => {
       );
     });
 
-    it("navigates to next page", () => {});
-    it("navigates to a specific page", () => {});
+    it("navigates to previous page", async () => {
+      const fetchingState = {
+        executeQuery: vi.fn(() =>
+          fromValue({
+            fetching: false,
+            data: {
+              characters: {
+                items: [...characterItems],
+              },
+            },
+          }),
+        ),
+      };
+
+      render(
+        <Provider value={fetchingState}>
+          <CharactersTable onCharacterSelect={() => {}} />
+        </Provider>,
+      );
+
+      const nextPageButton = await screen.findByTitle("Next Page");
+      await userEvent.click(nextPageButton);
+      const previousPageButton = await screen.findByTitle("Previous Page");
+      await userEvent.click(previousPageButton);
+
+      expect(fetchingState.executeQuery).toHaveBeenCalledTimes(3);
+      expect(fetchingState.executeQuery).toHaveBeenNthCalledWith(
+        3,
+        expect.objectContaining({
+          variables: {
+            filter: {},
+            page: 1,
+            pageSize: 50,
+          },
+        }),
+        {},
+      );
+    });
+
+    it.skip("navigates to a specific page", () => {});
+    it.skip("changes the page size", () => {});
   });
 
   describe("external state interactions", () => {
-    it("updates the films pie chart data", () => {});
-    it("updates the searchStore with loading state when is fetching data", () => {});
+    it("updates the films pie chart data", () => {
+      const fetchingState = {
+        executeQuery: () =>
+          fromValue({
+            fetching: false,
+            data: {
+              characters: {
+                items: characterItems,
+              },
+            },
+          }),
+      };
+
+      render(
+        <Provider value={fetchingState}>
+          <CharactersTable onCharacterSelect={() => {}} />
+        </Provider>,
+      );
+
+      expect(useChartsStore.getState().filmsPieChart.data).toMatchSnapshot();
+    });
+
+    it("updates the searchStore with loading state when is fetching data", () => {
+      const fetchingState = {
+        executeQuery: () => never,
+      };
+
+      render(
+        <Provider value={fetchingState}>
+          <CharactersTable onCharacterSelect={() => {}} />
+        </Provider>,
+      );
+
+      expect(useSearchStore.getState().isLoading).toBeTruthy();
+    });
   });
+
   describe("data fetching", () => {
-    it("fetches a page of the character table data", () => {});
-    it("fetches character data based on the search filters", () => {});
+    it("fetches character data based on the search filters", () => {
+      useSearchStore.getState().setIsLoading(true);
+      useSearchStore
+        .getState()
+        .setSearchFilter({ name: "Achilles", tvShow: "show" });
+
+      const fetchingState = {
+        executeQuery: vi.fn(() =>
+          fromValue({
+            fetching: false,
+            data: {
+              characters: {
+                items: [characterItems[0], characterItems[1]],
+              },
+            },
+          }),
+        ),
+      };
+
+      render(
+        <Provider value={fetchingState}>
+          <CharactersTable onCharacterSelect={() => {}} />
+        </Provider>,
+      );
+
+      expect(fetchingState.executeQuery).toHaveBeenCalledTimes(1);
+      expect(fetchingState.executeQuery).toHaveBeenCalledWith(
+        expect.objectContaining({
+          variables: {
+            filter: { name: "Achilles", tvShows: "show" },
+            page: 1,
+            pageSize: 500,
+          },
+        }),
+        {},
+      );
+    });
   });
 });
