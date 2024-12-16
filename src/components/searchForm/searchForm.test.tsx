c8de048b9e5bb42438dedd95630db5bbd46d13a1
Charidimos Tzedakis <harris.jedakis@gmail.com>
2024-12-16 10:17:22 +0200

    searchForm test cases

---
+++ src/components/searchForm/searchForm.test.tsx
@@ -0,0 +1,82 @@
+import { expect, describe, it } from "vitest";
+import { render, screen, waitFor } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+import useSearchStore from "@stores/searchStore";
+
+import SearchForm from "./index";
+
+describe("SearchForm", () => {
+  it("should render correctly", () => {
+    const { container } = render(<SearchForm />);
+
+    expect(container).toMatchSnapshot();
+  });
+
+  it("should render as disabled when data are loading", () => {});
+
+  describe("Search button", () => {
+    it("should be disabled if no text is typed in the form", () => {
+      render(<SearchForm />);
+      const searchButton = screen.getByRole("button", { name: /search/i });
+      expect(searchButton).toBeDisabled();
+    });
+
+    it("should be enabled if text is typed in characterName field", async () => {
+      render(<SearchForm />);
+      const searchButton = screen.getByRole("button", { name: /search/i });
+
+      const searchField = screen.getByLabelText(/character name/i);
+      await userEvent.type(searchField, "Achilles");
+
+      expect(searchButton).toBeEnabled();
+    });
+
+    it("should be enabled if text is typed in tvShow field", async () => {
+      render(<SearchForm />);
+
+      const searchButton = screen.getByRole("button", { name: /search/i });
+
+      const searchField = screen.getByLabelText(/tv show/i);
+      await userEvent.type(searchField, "tv show 1");
+
+      expect(searchButton).toBeEnabled();
+    });
+
+    it("should set the search filters in searchStore", async () => {
+      render(<SearchForm />);
+
+      const searchButton = screen.getByRole("button", { name: /search/i });
+      const searchFieldCharacterName = screen.getByLabelText(/character name/i);
+      const searchFieldTvShow = screen.getByLabelText(/tv show/i);
+
+      await userEvent.type(searchFieldCharacterName, "Achilles");
+      await userEvent.type(searchFieldTvShow, "tv show 1");
+      await userEvent.click(searchButton);
+
+      expect(useSearchStore.getState().searchFilter).toEqual({
+        name: "Achilles",
+        tvShow: "tv show 1",
+      });
+    });
+  });
+
+  describe("Clear fields button", () => {
+    it.only("should clear the form fields", async () => {
+      render(<SearchForm />);
+      const clearButton = screen.getByRole("button", { name: /Clear fields/i });
+      const searchFieldCharacterName = screen.getByLabelText(/character name/i);
+      const searchFieldTvShow = screen.getByLabelText(/tv show/i);
+
+      await userEvent.type(searchFieldCharacterName, "Achilles");
+      await userEvent.type(searchFieldTvShow, "tv show 1");
+      await userEvent.click(clearButton);
+
+      console.log(searchFieldCharacterName);
+
+      await waitFor(() => {
+        expect(searchFieldCharacterName).toHaveValue("");
+        expect(searchFieldTvShow).toHaveValue("");
+      });
+    });
+  });
+});
